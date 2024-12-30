54cee5c22dd12c36e6804692977b7a0b5b1597d2
N4dy4 <nadya741@icloud.com>
2024-12-29 22:02:13 -0500

    installing tailwind

---
+++ src/componets/title.jsx
@@ -0,0 +1,11 @@
+import react from "@vitejs/plugin-react-swc";
+
+const Title = () => {
+    return (
+        <>
+            <h1>Hello, My name is Nadya</h1>
+        </>
+    );
+};
+
+export default Title;
